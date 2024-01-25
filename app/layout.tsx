cc67a86f3072b86c199b1d0553151e7d4d81e7c1
gshah2020 <gaurang.r.shah@gmail.com>
2024-01-24 20:30:42 -0500

    chore: config react-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,12 +7,14 @@ import { ourFileRouter } from './api/uploadthing/core';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { ModalProvider } from '@/components/providers/modal-provider';
 
+import { QueryProvider } from '@/components/providers/query-provider';
+import { SocketProvider } from '@/components/providers/socket-provider';
+
 import { cn } from '@/lib/utils';
 
 import type { Metadata } from 'next';
 
 import './globals.css';
-import { SocketProvider } from '@/components/providers/socket-provider';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -48,8 +50,10 @@ export default function RootLayout({
             // disableTransitionOnChange
           >
             <SocketProvider>
-              <ModalProvider />
-              {children}
+              <QueryProvider>
+                <ModalProvider />
+                {children}
+              </QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
