0cac1639b05dc0e07268b066ac6d9fcaad4fb143
gshah2020 <gaurang.r.shah@gmail.com>
2024-01-25 19:33:37 -0500

    fix: add conditional check

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -50,7 +50,7 @@ export function ServerMember({ member, server }: ServerMemberProps) {
       <p
         className={cn(
           'text-sm font-semibold text-zinc-500 transition group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300',
-          params.memberId === member.id &&
+          params?.memberId === member.id &&
             'text-primary dark:text-zinc-200 dark:group-hover:text-white'
         )}
       >
